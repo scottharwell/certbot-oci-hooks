338c55b46901302c1094fe547424d917d6181b66
Scott Harwell <scott@harwell.me>
2021-03-15 11:42:27 -0400

    Seed OCI Hooks for certbot

---
+++ cleanup-hook.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env fish
+
+function error --argument message
+	set_color red
+    echo "❗️ $message"
+	exit 1
+end
+
+# If the OCI CLI does not exist on this system, then exit
+if not type -q oci
+	error "OCI CLI not present"
+end
+
+if set -q $OCI_COMPARTMENT_ID
+	error "OCI Compartment for DNS not set"
+end
+
+if set -q $DNS_ZONE
+	error "DNS_ZONE env variable must be set to update OCI DNS"
+end
+
+# Certbot env vars in case they are needed for reference
+#echo $CERTBOT_DOMAIN
+#echo $CERTBOT_VALIDATION
+#echo $CERTBOT_TOKEN
+#echo $CERTBOT_REMAINING_CHALLENGES
+#echo $CERTBOT_ALL_DOMAINS
+
+# Delete the DNS record that was created in the auth-hook script.
+echo "Deleting DNS entry"
+
+set OCI_DELETE_RECORD (
+	oci dns record rrset delete \
+	--force \
+	--compartment-id $OCI_COMPARTMENT_ID \
+	--zone-name-or-id $DNS_ZONE \
+	--domain "_acme-challenge.$CERTBOT_DOMAIN" \
+	--from-json "{\"rtype\":\"TXT\",\"rdata\":\"$CERTBOT_VALIDATION\"}"
+)
+
+echo "Cleanup script complete!"
